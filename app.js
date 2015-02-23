f1aa5168d7468064e057515062201700721b5689
Robooto <samrussell63@gmail.com>
2015-02-22 19:56:51 -0800

    Updated chat to store in mongodb

--- app.js
+++ app.js
@@ -2,53 +2,120 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var mongoose = require('mongoose');
+
+//another way
+// var io = require('socket.io').listen(app.listen(3000));
 
 // nickname array
-var nicknames = [];
+var users = {};
 
 server.listen(3000);
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+mongoose.connect('mongodb://localhost/socketchat', function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to mongodb');
+    }
+});
+
+var chatSchema = mongoose.Schema({
+    nick: String,
+    msg: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Chat = mongoose.model('Message', chatSchema);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 // connection for all io events
 io.sockets.on('connection', function(socket) {
-	
-	// receive nicknames and send them to the client if new
-	socket.on('new user', function (data, callback) {
-		if(nicknames.indexOf(data) !== -1) {
-			callback(false);
-		} else {
-			callback(true);
-			socket.nickname = data;
-			nicknames.push(socket.nickname);
-			updateNicknames();
-		}
+
+	// load old messages to the user
+	var query = Chat.find({});
+	query.sort('-created').limit(10).exec(function(err, docs) {
+		if(err) throw err;
+
+		socket.emit('load old msgs', docs);
 	});
 
-	// receiving message
-	socket.on('send message', function (data) {
-		// send message back to users
-		io.sockets.emit('new message', {
-			msg: data, nick: socket.nickname
-		});
+    // receive nicknames and send them to the client if new
+    socket.on('new user', function(data, callback) {
+        if (data in users) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            users[socket.nickname] = socket;
+            updateNicknames();
+        }
+    });
 
-		// broadcast sends to everyone execpt sender
-		//sockets.broadcast.emit('new message', data);
-	});
+    // receiving message
+    socket.on('send message', function(data, callback) {
 
-	//diconnect remove users
-	socket.on('disconnect', function (data) {
-		if(!socket.nickname) return;
+        var msg = data.trim();
+        if (msg.substr(0, 1) === '@') {
+            msg = msg.substr(1);
+            var ind = msg.indexOf(' ');
+            if (ind !== -1) {
+                var name = msg.substr(0, ind);
+                var msg = msg.substr(ind + 1);
+                if (name in users) {
+                    users[name].emit('whisper', {
+                        msg: msg,
+                        nick: socket.nickname
+                    });
+                    console.log('whisper');
+                } else {
+                    callback('Error!  Enter a valid user.');
+                }
 
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
-	});
+            } else {
+                callback('Error!  Please enter a message for your whisper.');
+            }
 
-	function updateNicknames() {
-		io.sockets.emit('usernames', nicknames);
-	}
+        } else {
+
+            // save to mongodb
+            var newMsg = new Chat({
+                msg: msg,
+                nick: socket.nickname
+            });
+            newMsg.save(function(err) {
+                if (err) throw err;
+
+                // send message back to users
+                io.sockets.emit('new message', {
+                    msg: msg,
+                    nick: socket.nickname
+                });
+            });
+
+
+            // broadcast sends to everyone execpt sender
+            //sockets.broadcast.emit('new message', data);
+        }
+
+    });
+
+    //diconnect remove users
+    socket.on('disconnect', function(data) {
+        if (!socket.nickname) return;
+
+        delete users[socket.nickname];
+        updateNicknames();
+    });
+
+    function updateNicknames() {
+        io.sockets.emit('usernames', Object.keys(users));
+    }
 
 });
-
